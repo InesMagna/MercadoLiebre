5210d659afb0579b9f6019efef3f24e02d4e7178
InesMagna <inemagnanini@gmail.com>
2022-09-19 21:33:50 -0300

    cambios

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const path = require ("path");
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor corriendo en puerto 3000")
+app.listen(process.env.PORT || 3030, () => {
+    console.log("Servidor corriendo en puerto 3030")
 });
 
 app.get('/', (req, res) =>{
-    res. sendFile(path.resolve(__dirname + './views/home.html'))
+    res. sendFile(path.resolve(__dirname, './views/home.html'))
 });
 
 app.get('/register', (req, res) =>{
-    res. sendFile(path.resolve(__dirname + './views/register.html'))
+    res. sendFile(path.resolve(__dirname, './views/register.html'))
 });
 
 app.get('/login', (req, res) =>{
-    res. sendFile(path.resolve(__dirname + './views/login.html'))
+    res. sendFile(path.resolve(__dirname, './views/login.html'))
 });
 
 app.post('/crearUsuario')
